334b780a7c39d0ae00235dfaafa738d922039322
WildHare95 <den.tsykalov@gmail.com>
2024-03-09 23:59:59 +0100

    Test task done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'usense-test-task' title`, () => {
+  it(`should have the 'usense-task' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('usense-test-task');
+    expect(app.title).toEqual('usense-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, usense-test-task');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, usense-task');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule, NgClass, NgOptimizedImage } from '@angular/common'
+import { AbstractControl, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+	imports: [ReactiveFormsModule, NgOptimizedImage, CommonModule],
 })
 export class AppComponent {
-  title = 'usense-test-task';
+	public passwordControl = new FormControl(null, [
+		Validators.required,
+		Validators.minLength(8),
+		this.passwordStrengthValidator,
+	])
+
+	passwordStrengthValidator(control: AbstractControl) {
+		const password = control.value
+
+		if (!password) {
+			return null
+		}
+
+		const onlyLetters = /[a-zA-Z]+/
+		const onlyDigits = /\d+/
+		const onlySymbols = /[^\w\s]+/
+
+		const containLetters = Boolean(password.match(onlyLetters))
+		const containDigits = Boolean(password.match(onlyDigits))
+		const containSymbols = Boolean(password.match(onlySymbols))
+
+		if (containLetters && containDigits && containSymbols) {
+			return { strongPassword: true, passwordStrength: 'strong' }
+		}
+
+		if (
+			(containLetters && containSymbols) ||
+			(containLetters && containDigits) ||
+			(containDigits && containSymbols)
+		) {
+			return { mediumPassword: true, passwordStrength: 'medium' }
+		}
+
+		if (containLetters || containDigits || containSymbols) {
+			return { easyPassword: true, passwordStrength: 'easy' }
+		}
+
+		return control.errors
+	}
 }
